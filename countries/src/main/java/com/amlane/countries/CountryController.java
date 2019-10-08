66e867c4a78107c88c7ebaa5da67c400f6ff3814
amlane <twelvexstring@gmail.com>
2019-10-07 22:38:02 -0500

    get countries by first letter and return in alphabetical order

--- countries/src/main/java/com/amlane/countries/CountryController.java
+++ countries/src/main/java/com/amlane/countries/CountryController.java
@@ -3,8 +3,11 @@ package com.amlane.countries;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/")
@@ -16,5 +19,14 @@ public class CountryController
     {
         CountriesProjectApplication.ourCountryList.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
         return new ResponseEntity<>(CountriesProjectApplication.ourCountryList.countryList, HttpStatus.OK);
+    }
+
+    //localhost:2019/names/start/{letter}
+    @GetMapping(value = "/names/start/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> getCountryByFirstLetter(@PathVariable char letter)
+    {
+        ArrayList<Country> rtnCountries = CountriesProjectApplication.ourCountryList.findCountries(c -> c.getName().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+        rtnCountries.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+        return new ResponseEntity<>(rtnCountries, HttpStatus.OK);
     }
 }
