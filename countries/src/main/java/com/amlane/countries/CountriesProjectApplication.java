e404a29d0d3cfbbab26df05c443edcff95d97949
amlane <twelvexstring@gmail.com>
2019-10-07 21:57:14 -0500

    set up country class and arraylist

--- countries/src/main/java/com/amlane/countries/CountriesProjectApplication.java
+++ countries/src/main/java/com/amlane/countries/CountriesProjectApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountriesProjectApplication
 {
+    static CountryList ourCountryList;
 
     public static void main(String[] args)
     {
+        ourCountryList = new CountryList();
         SpringApplication.run(CountriesProjectApplication.class, args);
     }
 
