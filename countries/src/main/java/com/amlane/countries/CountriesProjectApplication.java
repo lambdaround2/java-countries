a64e8e4070420ea7f06d4f8b9d7264debd67f4ef
amlane <twelvexstring@gmail.com>
2019-10-07 19:04:25 -0500

    initial commit

---
+++ countries/src/main/java/com/amlane/countries/CountriesProjectApplication.java
@@ -0,0 +1,15 @@
+package com.amlane.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CountriesProjectApplication.class, args);
+    }
+
+}
